d4448ea0880d1da343c21a24ff99ee5cbdf0c87e
y93x08y05 <1064807182@qq.com>
2018-09-04 11:02:59 +0800

    first commit

---
+++ src/test/java/DAO/redisConnection.java
@@ -0,0 +1,15 @@
+package DAO;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * Created by zhangta on 1/11/2018.
+ */
+public class redisConnection {
+    public static void main(String args[]){
+        Jedis test = new Jedis("127.0.0.1",6379);
+        test.set("hw","hello world");
+        String hello = test.get("hw");
+        System.out.println(hello);
+    }
+}
